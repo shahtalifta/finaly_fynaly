25784d6b0ebbdca229f348f57c7aca441e5d28f8
Roman Kofeynikov <r.kofeinikov@studiomobile.ru>
2019-10-08 19:21:03 +0300

    add test-cases for login form

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
 
-    parser.addoption('--language', action='store', default="es",
+    parser.addoption('--language', action='store', default="en",
                      help="Choose language: es or fr")
 
 
